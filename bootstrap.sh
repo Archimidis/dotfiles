a638dce26c872b115889b7c7eda9ea3f0e94a6b3
Nikolas Vourlakis <nvourlakis@gmail.com>
2016-07-26 04:26:05 +0300

    Create a simple installation script

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE}")" && pwd)"
+echo Creating symbolic links in $BASEDIR
+
+# vim
+echo Creating symbolic links for vim
+ln -s ${BASEDIR}/vim/.vimrc ~/.vimrc
+ln -s ${BASEDIR}/vim/.vim/ ~/.vim
+
+# zsh
+# echo Creating symbolic links for zsh
+# ln -s ${BASEDIR}/zshrc ~/.zshrc
+
+# git
+# echo Creating symbolic links for git
+# ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+
+# tmux
+# echo Creating symbolic links for tmux 
+# ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
+
